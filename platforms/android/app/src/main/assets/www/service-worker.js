712cc24627575f308711314fdbf1445ec01c8b10
Lehi Thomas <lehithomas25@gmail.com>
2019-04-03 20:26:08 -0500

    init commit

--- platforms/android/app/src/main/assets/www/service-worker.js
+++ platforms/android/app/src/main/assets/www/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8aec244cf3cb8abff40c3f604e776dac.js"
+  "./precache-manifest.ea4b3c24e1d7ebe1a1f61f3df68d3597.js"
 );
 
 workbox.clientsClaim();
